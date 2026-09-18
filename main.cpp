725877b7f782b750526c6e687534274e2975b4f9
Trung Hieu <letrunghieu037207@gmail.com>
2026-09-18 09:28:35 +0700

    u i

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    string text, word;
-    cout << "Enter sentence: ";
-    getline(cin, text);
-    cout << "Enter word to find: ";
-    cin >> word;
+    string fullName;
+    int birthYear;
 
-    size_t pos = text.find(word);
-    if (pos != string::npos) {
-        cout << "The word \"" << word << "\" is found at position " << pos << "." << endl;
-    } else {
-        cout << "The word \"" << word << "\" was not found." << endl;
+    cout << "Enter full name: ";
+    getline(cin, fullName);
+    cout << "Enter birth year: ";
+    cin >> birthYear;
+
+    // Loại bỏ khoảng trắng và chuyển thành chữ thường
+    string username = "";
+    for (char c : fullName) {
+        if (c != ' ') {
+            username += tolower(c);
+        }
     }
+
+    // Ghép năm sinh vào cuối username
+    username += to_string(birthYear);
+
+    cout << "Username: " << username << endl;
 
     return 0;
 }
